80ccaf37e202c6caf4d8baf0e81112bb73d4f031
leonwong03 <108467768+leonwong03@users.noreply.github.com>
2022-08-09 11:43:14 -0400

    commit tree e85fdb4f3a307fc2b8702f174e84807d8d022c91

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks (num1) {
+    if (num1 >= 42) {
+        return num1 - 42;
+    }else if(num1 < 42) {
+        return 42 - num1;
+    }
+} 
+
+function distanceFromHqInFeet (num1) {
+    return  distanceFromHqInBlocks (num1) * 264;
+}
+
+function distanceTravelledInFeet (num1, num2) {
+    if (num1 <= num2) {
+    return (num2 - num1) * 264;
+    }else if(num1 > num2) {
+        return (num1 - num2) * 264;
+    }
+}
+
+function calculatesFarePrice(start, destination) {
+    let distance = distanceTravelledInFeet(start, destination);
+    if (distance < 400) {
+    return 0;
+    } else if (distance > 400 && distance < 2000) {
+        return 2.56;
+    } else if (distance > 2000 && distance < 2500) {
+        return 25;
+    } else (distance > 2500) 
+        return 'cannot travel that far';
+}
